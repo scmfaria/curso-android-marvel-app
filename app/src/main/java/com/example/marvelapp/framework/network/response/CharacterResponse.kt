04786c1b6c2e8e56a74320ab27f031809336b6c4
scmfaria <sara.moura.faria@gmail.com>
2022-10-27 14:56:24 -0300

    Change API response

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,10 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.google.gson.annotations.SerializedName
 import com.sarafaria.core.domain.model.Character
 
 data class CharacterResponse(
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("thumbnail")
     val thumbnail: ThumbnailResponse
 )
 
